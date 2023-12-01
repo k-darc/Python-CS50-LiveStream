788f07289e7f429a6bd3b48950e1eecaaf7cdd2b
Kevin <neoglitch822@yahoo.com>
2023-12-01 16:49:42 -0500

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,16 +1,16 @@
-#use Speller, otherwise it won't run
+#use Speller, otherwise it won't run, python speller.py holmes.txt form Lecture 5.
 worlds = set()
 
 def check(word):
     return word.lower() in worlds
 
 def load(dictionary):
-    while open(dictionary) as file:
+    with open(dictionary) as file:
         words.update(file.read().splitlines())
     return True
 
 def size():
-    return  len(words)
+    return len(words)
 
 def unload(): # upload isn't needed in Python. Python manages memory for you, there is no Malloc, freeing or pointers
     return True
