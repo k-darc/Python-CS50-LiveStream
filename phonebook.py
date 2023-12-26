b158e9d60aa5f1a34d527794fef5ae98fbf902fd
Kevin <neoglitch822@yahoo.com>
2023-12-26 13:05:18 -0500

    dictionary
    simplified dict

--- phonebook.py
+++ phonebook.py
@@ -1,16 +1,13 @@
-people = [
-    {"name": "Carter","number": "+1-617-495-1000"},
-    {"name": "David", "number": "+1-617-495-1000"},
-    {"name": "John", "number": "+1-617-495-2750"},
-]
+people = {
+    "Carter": "+1-617-495-1000",
+    "David": "+1-617-495-1000",
+    "John": "+1-617-495-2750",
+}
 
 name = input("Name: ")
 
-for person in people:
-    if person["name"] == name:
-        number = person["number"]
-        print(f"Found {number}")
-        break
-
+if name in people:
+    number = people[name]
+    print(f"Found {number}")
 else:
-    print("Not found")
+    print("Not Found")
